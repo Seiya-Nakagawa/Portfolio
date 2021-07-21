da27a2e5fc2ad3ac49461380dcff9a343af694a0
SEIYA_NAKAGAWA <aibdlnew1.sn@gmail.com>
2021-07-22 08:09:53 +0900

    文言等修正 年齢自動計算追加

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-$(function() {
+// humbergerメニュー
+$(function () {
     $('.hamburger').click(function() {
         $(this).toggleClass('active');
- 
+
         if ($(this).hasClass('active')) {
             $('.globalMenuSp').addClass('active');
         } else {
@@ -9,3 +10,17 @@ $(function() {
         }
     });
 });
+
+// 年齢自動計算
+function getAge() {
+    // window.onload = function getAge() {
+    var today = new Date();
+    var birthday = new Date( 19880927 );
+    var tdate = ( today.getFullYear() * 10000 ) + (( today.getMonth() + 1 ) * 100 ) + today.getDate() ;
+    var ageR = Math.floor((tdate - birthday) / 10000);
+    // console.log( Math.floor(( tdate - birthday ) / 10000 )) ;
+    return ageR;
+}
+
+const age = document.getElementById('age');
+age.textContent = getAge() + "歳";
